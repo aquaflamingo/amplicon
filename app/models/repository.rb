fa787041aa0aefbf2b2a4c5a52222e7206b26afc
Robert <dev@robertsimoes.org>
2021-05-29 22:16:05 -0700

    Adds validations for name and description

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,2 +1,4 @@
 class Repository < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :description, presence: true, length: { minimum: 0, maximum: 140 }
 end
