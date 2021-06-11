a0dbd210fecba23120d8af82a24399cce9752b17
Robert <dev@robertsimoes.org>
2021-06-10 21:12:13 -0700

    Override Clearance::UsersController new and create
    * In order to render flash errors on failure of validity checks, we must
      override the UsersController that subclasses Clearance::UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,21 @@ class UsersController < Clearance::UsersController
   before_action :require_login, only: [:show]
   before_action :set_user, only: [:show]
 
+  def new
+    @user = user_from_params
+    render template: "users/new"
+  end
+
+  def create
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new", warning: @user.errors.full_messages.join(" ")
+    end
+
   def show
   end
 
