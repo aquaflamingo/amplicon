05a1af117954e3d4eb5fdb2c83ab3bd570c98075
Robert <dev@robertsimoes.org>
2021-06-10 21:01:33 -0700

    Adds username to Clearance user_from_params
    * These parameters are used by Clearance internally to construct User
      models for +new+ and +create+

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,17 @@ class UsersController < Clearance::UsersController
   def set_user
     @user = User.find(params[:id])
   end
+
+  # Parameters used to create a new user record via +new+ and +create
+  def user_from_params
+    email = user_params.delete(:email)
+    username = user_params.delete(:username)
+    password = user_params.delete(:password)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.username = username
+      user.email = email
+      user.password = password
+    end
+  end
 end
