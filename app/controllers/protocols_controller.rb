7b0e32f67f0dafbf9b4fad417c233d43a0838d64
robert <16901597+robertsimoes@users.noreply.github.com>
2021-06-12 22:20:18 -0700

    Adds Protocol Edit/Update (#12)
    * Adds ProtocolController#update and edit
    
    * Allows editing and updating for a particular portfolio
    
    * Adds protocols/edit.html.erb
    
    * Extract partial for `_form` from `new.html.erb` and reuse across
      `edit` and `new`
    
    * Adds Edit button to protocols/show.html.erb

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -1,7 +1,32 @@
 class ProtocolsController < ApplicationController
-  before_action :set_proto, only: [:show, :favorite]
+  before_action :set_proto, only: [:show, :edit, :favorite, :update]
   before_action :require_login
 
+  def index
+    @proto = current_user.protocols
+  end
+
+  def show
+  end
+
+  def new
+    @proto = current_user.protocols.new
+  end
+
+  def edit
+  end
+
+  #######################################################
+  # Write
+  #######################################################
+  def update
+    if @proto.update(proto_params)
+      redirect_to @proto, notice: "Updated!"
+    else
+      render template: 'edit'
+    end
+  end
+  
   def favorite 
     if favorite_params[:action_type] == "favorite"
       current_user.favorites << @proto
@@ -13,13 +38,6 @@ class ProtocolsController < ApplicationController
     redirect_to @proto
   end
 
-  def index
-    @proto = current_user.protocols
-  end
-
-  def show
-  end
-
   def create
     @proto = current_user.protocols.build(proto_params)
     if @proto.save
@@ -27,10 +45,6 @@ class ProtocolsController < ApplicationController
     else
       redirect_to new_protocol_path, warning: "Failed to create protocol"
     end
-  end
-
-  def new
-    @proto = current_user.protocols.new
   end
 
   private 
