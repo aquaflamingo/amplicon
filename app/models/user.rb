37878fd27f9ee982a0f24985449773e5d92994d4
Robert <dev@robertsimoes.org>
2021-06-12 22:51:19 -0700

    Sets up follower / following for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
   has_many :favorite_protocols # setup relationship
   has_many :favorites, through: :favorite_protocols, source: :protocol
 
+  # Who has followed this user
+  has_many :follower_relationships, foreign_key: "followed_id", class_name: "Relationship"
+  # Who has this user followed
+  has_many :followed_relationships, foreign_key: "follower_id", class_name: "Relationship"
+
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :following, through: :followed_relationships, source: :followed
+
   validates :username, 
     presence: true, 
     length: {minimum: 2, maximum: 20 },
