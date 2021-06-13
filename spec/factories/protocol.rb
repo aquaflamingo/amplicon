84d54ce0d2c15e3e1ce2c876eedb7c6ee8cf9735
Robert <dev@robertsimoes.org>
2021-06-12 21:17:50 -0700

    [Spec] Updates protocol factory

--- spec/factories/protocol.rb
+++ spec/factories/protocol.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :protocol, aliases: [:proto] do 
     sequence(:name) {|n| "name_#{n}"}
     description { "description" }
+    private { false }
+
+    association :user, factory: :user
   end
 end
