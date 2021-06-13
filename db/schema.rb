a8c731851d78965a015bf1fcd06fb7f0c5ff6762
Robert <dev@robertsimoes.org>
2021-06-12 20:28:21 -0700

    [Migration] Runs FavoriteProtocol migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_032643) do
+ActiveRecord::Schema.define(version: 2021_06_13_025550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2021_06_11_032643) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "favorite_protocols", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "protocol_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["protocol_id"], name: "index_favorite_protocols_on_protocol_id"
+    t.index ["user_id"], name: "index_favorite_protocols_on_user_id"
+  end
+
   create_table "protocols", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -80,5 +89,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_032643) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favorite_protocols", "protocols"
+  add_foreign_key "favorite_protocols", "users"
   add_foreign_key "protocols", "users"
 end
