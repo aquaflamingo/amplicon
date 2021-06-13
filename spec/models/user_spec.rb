ce1b0fa6790edf11a47d24c07efabe66c4298928
Robert <dev@robertsimoes.org>
2021-06-12 22:51:38 -0700

    [Spec] Adds following and follower examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
+  end
+
+  describe 'follow behaviour' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:another_user) { FactoryBot.create(:user) }
+
+    context 'when following another user' do
+      before do
+        FactoryBot.create(:relationship, follower: user, followed: another_user)
+      end
+
+      it 'has followed' do
+        expect(user.following.count).to eq(1)
+      end
+    end
+
+    context 'when followed' do
+      before do
+        FactoryBot.create(:relationship, follower: another_user, followed: user)
+      end
+
+      it 'has followers' do
+        expect(user.followers.count).to eq(1)
+      end
+    end
+  end
+
   describe 'validations' do
     describe 'username' do
       it 'validates format' do
