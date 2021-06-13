d6c9689c3e990876849c0fa0d2843312a8273b2b
Robert <dev@robertsimoes.org>
2021-06-12 21:10:14 -0700

    Adds render_visibility_badge to ProtocolsHelper

--- app/helpers/protocols_helper.rb
+++ app/helpers/protocols_helper.rb
@@ -2,4 +2,16 @@ module ProtocolsHelper
   def has_favorited?(user, protocol)
     user.favorites.exists?(protocol.id)
   end
+
+  def render_visibility_badge(proto)
+    if @proto.private?
+      content_tag :span, class: "badge rounded-pill bg-dark text-light" do
+        "Private"
+      end
+    else
+      content_tag :span, class: "badge rounded-pill  bg-light text-dark" do
+        "Public"
+      end 
+    end
+  end
 end
