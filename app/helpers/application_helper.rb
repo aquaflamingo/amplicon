a66537008e54f38933a71bf4218cc63206410d5b
Robert <dev@robertsimoes.org>
2021-06-27 12:47:49 -0700

    Adds audit_version_count to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def user_avatar(user, size=100)
     if user.avatar.attached?
       user.avatar.variant resize: "#{size}x#{size}"
@@ -11,5 +10,9 @@ module ApplicationHelper
   def gravatar_url(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
+  end
+
+  def audit_version_count(model)
+    model.audits.count
   end
 end
