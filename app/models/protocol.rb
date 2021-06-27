0a4be610bd79b153287deb7810dda62c1f7f2046
robert <16901597+robertsimoes@users.noreply.github.com>
2021-06-27 12:49:41 -0700

    Adds Audit Trail for Protocol Changes (#19)
    * Adds flashes for errors
    
    * Adds audited
    
    * Adds Audited JSONB migration
    
    * [Migration] Runs migration
    
    * Make protocol audited
    
    * Adds diffy gem
    
    * Adds audited with associated to Protocol
    
    * Render change history in Protocol show
    
    * Adds audit_version_count to ApplicationHelper
    
    * Adds AuditDiff to ProtocolsHelper
    
    * Adds history partial for protocols

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,16 +1,23 @@
 class Protocol < ApplicationRecord
+
+  VALID_NAME =  /\A(\w|\.)+\z/.freeze
+
   belongs_to :user
   has_many :favorite_protocols # setup relationship
 
   has_many :favorited_by, through: :favorite_protocols, source: :user
 
   # The type of protocol
-  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }, format: { with: VALID_NAME, message: 'Protocol can only include numbers, letters or underscores.'  }
   validates :description, presence: true, length: { minimum: 0, maximum: 140 }
 
   has_rich_text :reagents
   has_rich_text :equipment
   has_rich_text :materials
+
+  audited associated_with: :reagents
+  audited associated_with: :equipment
+  audited associated_with: :materials
 
   alias_attribute :owner, :user 
 end
