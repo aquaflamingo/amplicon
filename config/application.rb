8ba9cd10cc082f575c8d96ef14cde29840f04c41
Robert <dev@robertsimoes.org>
2021-06-29 20:11:47 -0700

    Refactors and modularizes application.html

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Amplicon
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.application_name = "Amplicon"
   end
 end
