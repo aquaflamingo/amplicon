dbbede7302694b598d7a9f6b644da9fc1f159c75
Robert <dev@robertsimoes.org>
2021-07-07 20:44:16 -0700

    Do not require login for terms, index and privacy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  before_action :require_login
+  before_action :require_login, except: %i[index terms_of_service privacy_policy]
 
   def index; end
   def terms_of_service;
