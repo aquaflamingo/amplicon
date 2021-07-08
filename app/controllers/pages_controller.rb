65475d05cfdcb9cfcd15d462b55bf0cbdf21ff02
Robert <dev@robertsimoes.org>
2021-07-07 19:59:31 -0700

    Adds TOS and Privacy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
   before_action :require_login
 
   def index; end
+  def terms_of_service;
+    render 'terms'
+  end
+  def privacy_policy
+    render 'privacy'
+  end
 end
